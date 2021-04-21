a26970f79a14c560ee8df96e34a605df841c3c34
michaelshaleen <shaleenmichael@yahoo.com>
2021-04-21 08:15:02 -0500

    header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function Header( {logo} ) {
+  return (
+    <>
+     <header >
+        <img src={logo} alt="credit one header" />
+      </header>
+      
+    </>
+  )
+}
+
+export default Header;
