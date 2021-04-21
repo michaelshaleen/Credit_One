f8ecc542addfa30fac39187ab63d4ad2a48ad57b
michaelshaleen <shaleenmichael@yahoo.com>
2021-04-21 15:42:20 -0500

    adding then going to focus on header rendering

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,21 +6,38 @@ import { Button } from '@material-ui/core';
 import '../App/App.css';
 // import Header from '../Header/Header';
 // import Accept from '../Accept_Btn/Accept';
+import Footer from '../Footer/Footer';
 import React, { useState } from 'react';
 
 
 function App() {
   const[header, setHeader] = useState(false);
+  const[img, setImg] = useState(false);
   const history = useHistory();
 
 
 
   const acceptBtn = () => {
     if(header){
-      setHeader(false)
+      setHeader(false);
+      setImg(true);
+
     }else{
-      setHeader(true)
+      setHeader(true);
+      setImg(true);
     }
+  }
+
+  if(img){
+    <header 
+        className={ header ? 'shown' : 'hidden' }>
+        <img src={logo} alt="credit one header" />
+      </header>
+  }else{
+    <header 
+        className={ header ? 'shown' : 'hidden' }>
+        <img src={white} alt="credit one header" />
+      </header>
   }
 
   const noBtn = () => {
@@ -39,47 +56,84 @@ function App() {
             <img src={white} alt="credit one in white text"/>
           </div>
       </header> */}
-      <div
-        className="text">
 
+      <div  
+        className="body">
+
+            
+          <div
+            className="text1">
+                <div>
+                  Consolidate Your Debt
+                </div>
+                <div>
+                    With A Lower Rate
+                </div>
+          </div>
+          
             <div
-              style={{fontSize: "30px"}}>Consolidate Your Debt
-              With A Lower Rate
-            </div>
-            <div>
-              XX.XX% 
-            </div>
-            <div>Manage life's expenses
-              by transferring a balance to
-              your Credit One Bank account today
-            </div>
-        </div>
+              className="text2">
+                <div
+                  className="percents">
 
-      <Button
-        style={{ backgroundColor: 'orange', color: 'white' }}
-        variant="contained" 
-        color="primary" 
-        onClick={acceptBtn}>
-          Accept Now
-     </Button>
+                  XX.XX%
+                </div>
+                  <div
+                    className="text3">
+                    <b>Promotional APR</b><br></br>
+                      now through<br></br>
+                      <b>XX/XX/XXXX</b>
+                    </div>
+                    <div class="vertical-divider">
+                        <p class="center-element"></p>
+                      </div>
+                    <div
+                      className="text4">
+                      A <b>XX.XX% variable APR</b><br></br>
+                      will apply after the<br></br>
+                      promotional period*
+                  </div>
+            </div>
+         
+
+              <div
+                className="text5">
+                Manage life's expenses by 
+                transferring a balance
+                to your Credit One Bank 
+                account today
+              </div>
+                
+          
+
+          <Button
+            style={{ 
+              backgroundColor: 'orange', 
+              color: 'white',
+              margin: '10px'}}
+            variant="contained" 
+            color="primary" 
+            onClick={acceptBtn}>
+              Accept Now
+        </Button>
 
 
-      <Button
-        style={{ backgroundColor: 'grey', color: 'white' }}
-        variant="contained" 
-        color="primary"
-        onClick={noBtn}>
-          No Thanks
-      </Button>
-      <p
-        className="text">
-          A fee may apply.
-      </p>
-      <footer>
-        *See the *Disclosures and Terms Conditions'
-        on the application page by selecting 
-        the 'Accept Now' button.
-      </footer>
+          <Button
+            style={{ backgroundColor: 'grey', color: 'white' }}
+            variant="contained" 
+            color="primary"
+            onClick={noBtn}>
+              No Thanks
+          </Button>
+          <p
+            className="text"
+            style={{fontSize: "12px"}}>
+              A fee may apply.
+          </p>
+          <Footer />
+
+      </div>
+      
 
      
     </>
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+      <footer>
+        *See the 'Disclosures and Terms Conditions'
+        on the application page by selecting 
+        the 'Accept Now' button.
+      </footer>
+      
+    </>
+  )
+}
+
+export default Footer
